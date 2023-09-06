79c2aadfeeb4532add95af42cb0b50f1a98278fe
Denis Sersun <d.sersun@gmail.com>
2023-09-06 18:29:02 +0300

    changed to 2 as per comment

--- src/main/java/org/sersun/Main.java
+++ src/main/java/org/sersun/Main.java
@@ -2,6 +2,6 @@ package org.sersun;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println ("Hello World!1");
+        System.out.println ("Hello World!2");
     }
 }
