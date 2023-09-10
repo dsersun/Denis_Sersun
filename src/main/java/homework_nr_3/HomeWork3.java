5b19796281e6436a7a3cab100ba9de108a76b024
Denis Sersun <d.sersun@gmail.com>
2023-09-10 20:36:36 +0300

    Send homework nr. 2

---
+++ src/main/java/homework_nr_3/HomeWork3.java
@@ -0,0 +1,48 @@
+package homework_nr_3;
+
+public class HomeWork3 {
+    public static void main (String[] args) {
+        // 1 part
+        int day = 1;
+        switch (day){
+            case 1:
+                System.out.println ("Январь");
+                break;
+            case 2:
+                System.out.println ("Февраль");
+                break;
+            case 3:
+                System.out.println ("Март");
+                break;
+            case 4:
+                System.out.println ("Апрель");
+                break;
+            case 5:
+                System.out.println ("Май");
+                break;
+            case 6:
+                System.out.println ("Июнь");
+                break;
+            case 7:
+                System.out.println ("Июль");
+                break;
+            case 8:
+                System.out.println ("Август");
+                break;
+            case 9:
+                System.out.println ("Сентябрь");
+                break;
+            case 10:
+                System.out.println ("Октябрь");
+                break;
+            case 11:
+                System.out.println ("Ноябрь");
+                break;
+            case 12:
+                System.out.println ("Декабрь");
+                break;
+            default:
+                System.out.println ("Число не соответствует номеру месяца!");
+        }
+    }
+}
