50becc51a0afbee3aba4d0f7e989dee5077b73db
Denis Sersun <d.sersun@gmail.com>
2023-09-27 23:01:42 +0300

    Revert "Homework nr 9 is done."
    This reverts commit 5e5eb0fd6d0d44dc0c6b400787982f735cba670c.

--- src/main/java/homework_nr_9/Erbivor.java
+++
@@ -1,9 +0,0 @@
-package homework_nr_9;
-
-public abstract class Erbivor {
-    abstract void eatFruit (String fruit);
-
-    public void collectNuts () {
-        System.out.println ("Animals collect nuts!");
-    }
-}
